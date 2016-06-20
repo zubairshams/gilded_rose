467a39116d24166b7c970a1bb3e66a2e5433a3d8
Zubair Shams <email.to.zubi@gmail.com>
2016-06-21 00:43:29 +0800

    item updater sub types

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -24,6 +24,37 @@ class ItemUpdater
   end
 end
 
+class AgedBrieUpdater < ItemUpdater
+  def update_quality
+    @item.quality += @item.sell_in > 0 ? 1 : 2
+  end
+end
+
+class BackstageUpdater < ItemUpdater
+  def update_quality
+    @item.quality += if @item.sell_in > 10
+                       1
+                     elsif item.sell_in > 5
+                       2
+                     elsif item.sell_in > 0
+                       3
+                     else
+                       - @item.quality
+                     end
+  end
+end
+
+class SulfurasUpdater < ItemUpdater
+  def update_quality; end
+  def update_sell_in; end
+end
+
+class ConjuredUpdater < ItemUpdater
+  def update_quality
+    2.times {super}
+  end
+end
+
 ######### DO NOT CHANGE BELOW #########
 
 Item = Struct.new(:name, :sell_in, :quality)
